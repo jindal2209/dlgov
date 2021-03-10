e8a3602459b0dec10ea2417243ebc3876ba17715
jindal2209 <shubham7811@yahoo.com>
2021-03-10 12:00:57 +0530

    ''

--- form/admin.py
+++ form/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Complaint
 
 # Register your models here.
-admin.site.register(Complaint)
+@admin.register(Complaint)
+class ComplaintView(admin.ModelAdmin):
+	list_display = ('complaint_number',"phone_number","status",'district','ward')
+	list_filter = ("status",'district','ward')
+	search_fields = ('complaint_number','phone_number')
